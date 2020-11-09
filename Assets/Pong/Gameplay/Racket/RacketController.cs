933c39ea1b89e0af69919dbe6c0b575a2de24942
Utz Stauder <utzstauder@users.noreply.github.com>
2020-11-09 10:05:59 +0100

    added racket controller script

--- Assets/Pong/Gameplay/Racket/RacketController.cs
+++ Assets/Pong/Gameplay/Racket/RacketController.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RacketController : MonoBehaviour
 {
+    float verticalInput;
+    public float movementSpeed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello world!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 1. read input
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // 2. update object state
+        transform.position += Vector3.up * verticalInput * movementSpeed * Time.deltaTime;
+
+        // (3. communicate updated state)
     }
 }
