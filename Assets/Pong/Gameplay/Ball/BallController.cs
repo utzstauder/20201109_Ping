228aa5dfd8fc696f1320f666f63b9fba65c1b2aa
Utz Stauder <utzstauder@users.noreply.github.com>
2020-11-09 11:18:14 +0100

    Added ball

---
+++ Assets/Pong/Gameplay/Ball/BallController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+
+    public float initialSpeed = 5f;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        rigidbody2D.velocity = Random.insideUnitCircle.normalized * initialSpeed;
+    }
+}
