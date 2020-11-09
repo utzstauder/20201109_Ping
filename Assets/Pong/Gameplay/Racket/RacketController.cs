2ad8e4dcafa7e16fa9b1dbcbe8bbb7a495085902
Utz Stauder <utzstauder@users.noreply.github.com>
2020-11-09 11:01:01 +0100

    added walls and physics

--- Assets/Pong/Gameplay/Racket/RacketController.cs
+++ Assets/Pong/Gameplay/Racket/RacketController.cs
@@ -5,21 +5,29 @@ public class RacketController : MonoBehaviour
     float verticalInput;
     public float movementSpeed = 5f;
 
-    // Start is called before the first frame update
-    void Start()
+    public string axisName;
+
+    private Rigidbody2D rigidbody2D;
+
+    private void Awake()
     {
-        Debug.Log("Hello world!");
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // 1. read input
-        verticalInput = Input.GetAxisRaw("Vertical");
+        verticalInput = Input.GetAxisRaw(axisName);
 
         // 2. update object state
-        transform.position += Vector3.up * verticalInput * movementSpeed * Time.deltaTime;
+        // transform.position += Vector3.up * verticalInput * movementSpeed * Time.deltaTime;
 
         // (3. communicate updated state)
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = Vector2.up * verticalInput * movementSpeed;
     }
 }
