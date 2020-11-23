11f2be965a696600b52c2fe12d6b057f4af923f2
Utz Stauder <utzstauder@users.noreply.github.com>
2020-11-23 09:37:25 +0100

    added AI controller for rackets

--- Assets/Pong/Gameplay/Racket/RacketController.cs
+++ Assets/Pong/Gameplay/Racket/RacketController.cs
@@ -6,24 +6,51 @@ public class RacketController : MonoBehaviour
     public float movementSpeed = 5f;
 
     public string axisName;
+    public bool aiControlled;
+    public Transform aiMovementTarget; // default = null
 
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rigidbody2D; // default = null
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // 1. read input
-        verticalInput = Input.GetAxisRaw(axisName);
+        if (aiControlled)
+        {
+            if (aiMovementTarget != null)
+            {
+                // ball.y > this.y
+                if (aiMovementTarget.position.y > transform.position.y)
+                {
+                    // move up
+                    verticalInput = 1f;
+                } else
+                {
+                    // move down
+                    verticalInput = -1f;
+                }
+            } else
+            {
+                //GameObject targetObject = GameObject.Find("Ball");
 
-        // 2. update object state
-        // transform.position += Vector3.up * verticalInput * movementSpeed * Time.deltaTime;
+                BallController ball = GameObject.FindObjectOfType<BallController>();
 
-        // (3. communicate updated state)
+                if (ball != null)
+                {
+                    aiMovementTarget = ball.transform;
+                } else
+                {
+                    Debug.LogError("AiMovementTarget has not been assigned!");
+                }
+            }
+
+        } else
+        {
+            verticalInput = Input.GetAxisRaw(axisName);
+        }
     }
 
     private void FixedUpdate()
